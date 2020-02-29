f868cc6535c6255a9fe14e62c2fbe9753d791651
Ilya Puchka <ilyapuchka@gmail.com>
2020-02-29 14:50:37 +0000

    initial commit

---
+++ Currency/Currency/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+
+        return true
+    }
+
+}
+
